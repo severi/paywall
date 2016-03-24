446fee82784dbbeaee9f2c1d1633a6365e0b7253
Severi Haverila <severi.haverila@gmail.com>
2016-03-24 11:40:22 +0100

    stuff

--- paywall.user.js
+++ paywall.user.js
@@ -9,10 +9,15 @@
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.js
 // @author      severi
 // @run-at      document-end
+// @downloadURL https://github.com/severi/paywall/blob/master/paywall.user.js
+// @supportURL  https://github.com/severi/paywall
+// @source      https://github.com/severi/paywall
 // ==/UserScript==
 
 $( document ).ready(function() {
     $("#article-text p").css("display", "block");
-    $("#paywall").css("display", "none");
     $("div#content div").css("display", "block");
+    $("#article-speaker-content").css("display", "none");
+    $("#paywall").css("display", "none !important");
+    $("#paywall *").css("display", "none !important");
 });
